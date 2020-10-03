c6394370d1e3a26de22fb22700ef4ae0d2dec213
moonyejin <mjin1102@dongguk.edu>
2020-10-03 18:05:56 +0900

    질문답변 페이지 완성

--- mypage/answerQuestions/urls.py
+++ mypage/answerQuestions/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name='answerQuestions'
+
 urlpatterns=[
     path('',views.index2),
+    path('<int:question_id>/',views.detail, name="detail"),
+    path('answer/create/<int:question_id>',views.answerCreate,name="answerCreate"),
+
 ]
--- mypage/answerQuestions/views.py
+++ mypage/answerQuestions/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 # Create your views here.
-from .models import MdlQuestion
+from .models import Question
 
 def index2(request):
-    questLists=MdlQuestion.objects.filter(id=1)
+    questLists=Question.objects.all()
     return render(request, 'index2.html', {'questLists':questLists})
+
+def detail(request, question_id):
+    question=Question.objects.get(q_id=question_id)
+    context={'question':question}
+    return render(request,'detailInfo.html',context)
+
+def answerCreate(request, question_id):
+    question_answer=Question.objects.get(q_id=question_id)
+    answer=request.POST['answer']
+    question_answer.answer=answer
+    question_answer.save()
+    questLists=Question.objects.all()
+    """return render(request, 'index2.html',{'questLists':questLists})"""
+    return redirect('answerQuestions:detail',question_id=question_answer.q_id)
+
+
