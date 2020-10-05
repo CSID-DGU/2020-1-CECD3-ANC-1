12ca678c125604d3f6c4794dee98493fdea317ff
moonyejin <mjin1102@dongguk.edu>
2020-10-05 23:16:44 +0900

    오류 수정

--- mypage/chatbot/views.py
+++ mypage/chatbot/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def index3(request):
-    return render(request, 'chatbot/index3.html')
+    """if request.session.get('user',False) :
+        user=(MdlUser.objects.get(username=request.session.get('user',False)))
+        userid=user.id
+        if ((MdlRoleAssignments.objects.get(userid=userid)).roleid) == 4 :
+            teachList = MdlEnrolFlatfile.objects.filter(userid=userid)
+            context = {'teachList':teachList}
+            return render(request, 'chatbot/index3.html',context)
+        else :
+            return render(request, 'chatbot/index3.html')"""
+    return render(request, 'chatbot/index3.html',)
