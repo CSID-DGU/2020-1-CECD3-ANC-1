4032d2fe8cd9aa6946aaa02ac98b06b77e490be5
lee minju <dptmfkdls120@gmail.com>
2020-10-06 12:33:18 +0900

    kobaksa in the Django

--- mypage/chatbot/urls.py
+++ mypage/chatbot/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
+    path('chat/', views.chat_view, name='chat-home'),
     path('',views.index3, name='index3'),
 ]
