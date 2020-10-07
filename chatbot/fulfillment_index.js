9ebc2c806b4bb33cccb9d067495b75e6035d010f
lee minju <dptmfkdls120@gmail.com>
2020-10-07 12:05:49 +0900

    edit CH1_CS

--- chatbot/fulfillment_index.js
+++ chatbot/fulfillment_index.js
@@ -38,7 +38,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
 /* MySQL 연결 정보 */
 function connectToDatabase(){
   const connection = mysql.createConnection({
-  	host:'52.78.240.248',
+  	host:'', // 보안을 위해..
     user:'moodle',
     password:'1234',
     database:'moodle'
@@ -106,19 +106,19 @@ function connectToDatabase(){
     var answer; var check;
     check = "더 자세한 내용은 1단원 교안 8 ~ 13 페이지를 참고하세요";
     if(parameter.compiler[0] == "compiler" && parameter.compiler[1] == "cross-compiler" && parameter.different == "차이점"){
-        answer = "컴파일러는 고급언어로 쓰인 프로그램을 컴퓨터에서 바로 실행될 수 있는 형태의 목적 프로그램으로 바꾸어 주는 번역기고, 크로스 컴파일러는 소스 프로그램을 다른 기종에 대한 기계어로 번역하는 컴파일러입니다.";
+        answer = "컴파일러는 고급언어로 쓰인 프로그램을 컴퓨터에서 바로 실행될 수 있는 형태의 목적 프로그램으로 바꾸어 주는 번역기고, 크로스 컴파일러는 소스 프로그램을 다른 기종에 대한 기계어로 번역하는 컴파일러입니다."+check;
     } else if(parameter.compiler == "compiler" && parameter.interpreter == "interpreter" && parameter.different == "차이점"){
-        answer = "컴파일러는 고급언어로 쓰인 프로그램을 컴퓨터에서 바로 실행될 수 있는 형태의 목적 프로그램으로 바꾸어 주는 번역기고, 인터프리터는 고급언어로 작성된 코드를 한 단계씩 해설해서 실행시키는 방법입니다.";
+        answer = "컴파일러는 고급언어로 쓰인 프로그램을 컴퓨터에서 바로 실행될 수 있는 형태의 목적 프로그램으로 바꾸어 주는 번역기고, 인터프리터는 고급언어로 작성된 코드를 한 단계씩 해설해서 실행시키는 방법입니다."+check;
     } else if(parameter.compiler == "compiler" && parameter.structure == "structure"){
-      answer = "일반적인 컴파일러 구조는 \"어휘 분석기 (토큰)> 구문 분석기(트리) > 중간 코드 생성기(중간 코드) > 코드 최적화(최적화된 코드) > 타겟 코드 생성기\" 입니다. 그림과 ";
+      answer = "일반적인 컴파일러 구조는 \"어휘 분석기 (토큰)> 구문 분석기(트리) > 중간 코드 생성기(중간 코드) > 코드 최적화(최적화된 코드) > 타겟 코드 생성기\" 입니다. 그림과 "+check;
     } else if(parameter.compiler == "compiler"){
-        answer = "컴파일러는 고급언어로 쓰인 프로그램을 컴퓨터에서 바로 실행될 수 있는 형태의 목적 프로그램으로 바꾸어 주는 번역기입니다.";
+        answer = "컴파일러는 고급언어로 쓰인 프로그램을 컴퓨터에서 바로 실행될 수 있는 형태의 목적 프로그램으로 바꾸어 주는 번역기입니다."+check;
     } else if(parameter.interpreter == "interpreter"){
-        answer = "인터프리터는 고급언어로 작성된 코드를 한 단계씩 해설해서 실행시키는 방법입니다.";
+        answer = "인터프리터는 고급언어로 작성된 코드를 한 단계씩 해설해서 실행시키는 방법입니다."+check;
     } else if(parameter.preprocessor == "preprocessor"){
-      answer = "프리프로세서, 즉 전처리기는 소스 프로그램을 확장된 소스 프로그램으로 바꿔줍니다. 이를 translator에서 target 프로그램으로 바꾸는 것입니다.";
+      answer = "프리프로세서, 즉 전처리기는 소스 프로그램을 확장된 소스 프로그램으로 바꿔줍니다. 이를 translator에서 target 프로그램으로 바꾸는 것입니다."+check;
   }
-    agent.add(answer+check);
+    agent.add(answer);
   }
 
   // 컴파일러 구조
