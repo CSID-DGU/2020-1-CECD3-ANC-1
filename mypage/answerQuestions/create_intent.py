17cf624dafac26f10c02ed8096d547a68caf02b0
lee minju <dptmfkdls120@gmail.com>
2020-10-19 17:41:59 +0900

    Add Training_phrases_existing_intent function

--- mypage/answerQuestions/create_intent.py
+++ mypage/answerQuestions/create_intent.py
@@ -1,13 +1,16 @@
 import dialogflow_v2 as dialogflow
 import os
 
-def create_intent():
+def dialogflow_setting():
   GOOGLE_AUTHENTICATION_FILE_NAME = "kobaksa.json"
   current_directory = os.path.dirname(os.path.realpath(__file__))
   path = os.path.join(current_directory, GOOGLE_AUTHENTICATION_FILE_NAME)
   os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = path
   intents_client = dialogflow.IntentsClient()
   parent = intents_client.project_agent_path('kobaksa-1b59d')
+
+def create_intent():
+  dialogflow_setting()
 
   part = dialogflow.types.Intent.TrainingPhrase.Part(
     text = "유소영의 테스트")
@@ -40,3 +43,32 @@ def create_intent():
   )
 
   response = intents_client.create_intent(parent, intent)
+
+# 매개변수 설정해서 코드 수정 필요
+# 해당 인텐트에 trainging phrases 추가하기
+# 이와 같이 코드로 default 답을 수정 할 수도 있을듯
+def update_intent():
+  dialogflow_setting()
+
+  intents=intents_client.list_intents(parent)
+  intent_path=[
+      intent.name for intent in intents
+      if intent.display_name=="getDataFromMySQL"
+  ]
+  print(intent_path)
+  training_phrases = []
+  intent= intents_client.get_intent(intent_path[0], intent_view=dialogflow.enums.IntentView.INTENT_VIEW_FULL)
+  #intent = intents_client.get_intent(intent_path[0])
+
+  part = dialogflow.types.Intent.TrainingPhrase.Part(
+      text = "업데이트 확인입니다!!!!")
+  training_phrase = dialogflow.types.Intent.TrainingPhrase(parts=[part])
+  training_phrases.append(training_phrase)
+  # training_phrases가 여러개일 경우
+  '''for training_phrases_part in training_phrases_parts:
+        part = dialogflow.types.Intent.TrainingPhrase.Part(
+            text=training_phrases_part)
+        training_phrase = dialogflow.types.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)'''
+  intent.training_phrases.extend(training_phrases)
+  response  = intents_client.update_intent(intent, language_code='ko')
