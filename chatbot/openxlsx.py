d30eba7ad870f2bd01d26aeb8887c3f3d6dcf150
0meeee <bbang5368@gmail.com>
2020-10-22 13:52:05 +0900

    update openxlsx.py

--- chatbot/openxlsx.py
+++ chatbot/openxlsx.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import openpyxl
-
+import re
 
 #한 건의 대화에 대한 정보를 담는  객체
 class Conversation:
@@ -22,18 +22,30 @@ class Conversation:
             self.answer = textA_mod
         return "질문 : " + str(self.question) + "\n대답 : " + str(self.answer)
     
-wb = openpyxl.load_workbook(u"qnalist.xlsx")
+wb = openpyxl.load_workbook(u"qnalist.xlsx", data_only = True)
 
 ws = wb.active
 
 conversations = []
 
 #시트내 존재하는 모든 데이터 객체로 담음
-for r in ws.rows:
-    c = Conversation(r[0].value, r[1].value, r[2].value, r[3].value)
+try:
+    for r in ws.rows:
+#엑셀 시트내 큰따옴표 제거   
+        if r[0].value is not None:
+            q = r[2].value
+            qq = re.sub("[\"]", "", str(q))
+        
+            a = r[3].value
+            aa = re.sub("[\"]", "", str(a))
+        
+            c = Conversation(r[0].value, r[1].value, qq, aa)
     
-    conversations.append(c)
-
+            conversations.append(c)
+        else: continue
+except Exception as e:
+    print(e)
+        
 wb.close()
 
 for c in conversations:
